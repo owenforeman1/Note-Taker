28139a7ea3cf15e0c85d86a696326349c02e8fb3
Owen Foreman <owen4man@gmail.com>
2022-04-13 19:21:04 -0400

    notes save and can be viewed

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require("path");
@@ -8,19 +9,41 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
-
-  console.info(`${req.method}`);
 });
 
 app.get("/api/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
-
 app.post("/api/notes", (req, res) => {
-  console.log(req.body);
-  res.send("bobobobobobobobo")
+  if (req.body && req.body.title) {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const newData = JSON.parse(data);
+        newData.push(req.body);
+        console.log(newData);
+        idSetter(newData);
+        fs.writeFile("./db/db.json", JSON.stringify(newData), (err) => {
+          if (err) console.log(err);
+          else {
+            console.log("File written successfully\n");
+            console.log("The written has the following contents:");
+          }
+        });
+      }
+    });
+  }
+  res.send("hello");
 });
+
+function idSetter(newData) {
+  for (let index = 0; index < newData.length; index++) {
+    const element = newData[index];
+    element.id = index
+  }
+}
 
 app.delete("/api/notes/:id", (req, res) => {
   res.send(req.params);
